03e935dcde630097b7d37fc55297edb33f234e09
littlechihuo <119745730+littlechihuo@users.noreply.github.com>
2025-04-23 09:01:18 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["littlechihuo.uicp.vip"];
+pub const RS_PUB_KEY: &str = "KUfJqe3CyGer7CfiHDF8qdbV5Lx4p8EuANxm0dlCS8c=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
